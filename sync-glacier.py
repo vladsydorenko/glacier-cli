ca108c26e0c2fb1dccd87ec87e6b2d5914640341
Vlad Sydorenko <vladyslav.sydorenko@gmail.com>
2015-06-17 22:25:29 +0300

    Added archives retrieving + some minor features

--- sync-glacier.py
+++ sync-glacier.py
@@ -3,7 +3,7 @@ from boto.utils import parse_ts
 from boto.glacier import connect_to_region
 from boto.glacier.layer2 import Layer2
 from boto.glacier.exceptions import UploadArchiveError
-from datetime import date
+from datetime import date, datetime
 import zipfile
 import sys
 import os
@@ -11,6 +11,7 @@ import json
 import datetime
 import time
 import argparse
+import traceback
 
 # Outputs the config file
 def write_config_file():
@@ -19,10 +20,15 @@ def write_config_file():
 		f.write(secret_key + "\n")
 		f.write(vault_name + '|' + region + "\n")
 		f.write('|'.join(dirs) + "\n")
-		f.write(inventory_job + "\n")
-		f.write(ls_present + "\n")
-		for name, data in ls.iteritems():
+		f.write(inventory_retrieval_job_id + "\n")
+		f.write(inventory_present + "\n")
+		f.write(str(len(inventory)) + "\n")
+		for name, data in inventory.iteritems():
 			f.write(name + '|' + str(data['id']) + '|' + str(data['size']) + "\n")
+
+		f.write(get_job_present + "\n")
+		for name, data in get_jobs.iteritems():
+			f.write(name + '|' + str(data['job_id']) + "\n")
 
 def format_bytes(bytes):
 	for x in ['bytes', 'KB', 'MB', 'GB']:
@@ -49,10 +55,15 @@ def zipdir(path, ziph):
 
 def generate_archive_name(dir):
 	split_dir = dir.split('/')
-	name = date.today().strftime("%d_%m_%Y") + "__" + split_dir[-1] + ".zip";
+	name = date.today().strftime("%d-%m-%Y__%H-%M") + "__" + split_dir[-1] + ".zip";
 	return name
 
-config = "config.f"	
+def print_vault_list(v_list):
+	for vault in v_list:
+		print (" " if vault.name != vault_name else '*') + vault.name
+
+
+config = "config_nongit.f"	
 # Make sure the config file exists
 if not os.path.exists(config):
 	print "Config file not found. Pass in a file with the vault name and the directory to sync on separate lines."
@@ -68,26 +79,45 @@ with open(config, 'rU') as f:
 	region 	   = vault_info[1]
 	
 	dirs = f.readline().strip().split('|')
-	inventory_job = f.readline().strip()
-	ls_present = f.readline().strip()
+	inventory_retrieval_job_id = f.readline().strip()
+	inventory_present = f.readline().strip()
+	inventory_size    = f.readline().strip()
+	if not inventory_size:
+		inventory_size = '0'
 
-	
-	ls = {}
-	for file in f.readlines():
+	inventory = {}
+	for i in range(int(inventory_size)):
+		file = f.readline();
 		name, id, size = file.strip().split('|')
-		ls[name] = {
+		inventory[name] = {
 			'id': id,
 			'size': int(size)
 		}
 
+	get_jobs = {}
+	get_job_present = f.readline().strip()
+	for file in f.readlines():
+		name, id = file.strip().split('|')
+		get_jobs[name] = {
+			'job_id' : id
+		}
+
+# Check if command line argument was passed
 if len(sys.argv) < 2:
 	print r"You need to give a job specifier e.g. -b or -dv or etc."
 	sys.exit(1)
 
+# Parse command line arguments
 parser = argparse.ArgumentParser(description='Command line interface for Amazon Glacier')
-parser.add_argument('-c','--create', help='Create new vault', action="store_true")
-parser.add_argument('-dv','--delete', help='Delete vault', action="store_true")
-parser.add_argument('-b','--backup', help='Immediately backup data', action="store_true")
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-c', '--create',   help='Create new vault', action="store_true")
+group.add_argument('-dv','--delete',   help='Delete vault', action="store_true")
+group.add_argument('-b', '--backup',   help='Immediately backup data', action="store_true")
+group.add_argument('-i', '--inventory',help='Initialize invertory retrieving job', action="store_true")
+group.add_argument('-g', '--get', 	   help='Get vault', action="store_true")
+group.add_argument('-gl','--get_last', help='Get only last backup', action="store_true")
+group.add_argument('-lv', '--list_vaults',help='List all vaults', action="store_true")
+group.add_argument('-lj','--list_jobs',help='List all running jobs', action="store_true")
 arg = parser.parse_args()
 
 # Check some of the values in the config file
@@ -112,46 +142,63 @@ for dir in dirs:
 connect_to_region(vault_info[1], aws_access_key_id=access_key_id, aws_secret_access_key=secret_key)
 glacier = Layer2(aws_access_key_id=access_key_id, aws_secret_access_key=secret_key, region_name=region)
 
-if arg.backup or arg.delete:
-	vault = glacier.get_vault(vault_name)
-else:
+if arg.list_vaults:
+	print_vault_list(glacier.list_vaults())
+	sys.exit()
+elif arg.create:
 	print "Creating vault " + vault_name
 	vault = glacier.create_vault(vault_name)
+else:
+	vault = glacier.get_vault(vault_name)
 
-# Ah, we don't have a vault listing yet. 
-if not ls_present and not arg.create:
+# If we need to list all jobs
+if arg.list_jobs:
+	list_jobs = vault.list_jobs()
+	for job in list_jobs:
+		print job.creation_date + " " + str(job.action) + " " + str(job.id) + " " + str(job.completion_date) + " " + str(job.status_message) 
+	sys.exit(0)
+
+# Ah, we don't have a vault inventory listing yet. 
+if not inventory_present and not arg.create:
 	# No job yet? Initiate a job.
-	if not inventory_job:
-		inventory_job = vault.retrieve_inventory()
+	
+	if not inventory_retrieval_job_id:
+		inventory_retrieval_job_id = vault.retrieve_inventory()
 		write_config_file()
-		print "Requested an inventory. This usually takes about four hours."
+		print "Requested an inventory. This usually takes about five hours."
 		sys.exit(0)
 	
 	# We have a job, but it's not finished.
-	job = vault.get_job(inventory_job)
+	job = vault.get_job(inventory_retrieval_job_id)
+	print "Job has been started: " + job.creation_date
 	if not job.completed:
-		print "Waiting for an inventory. This usually takes about four hours."
+		print "Waiting for an inventory. This usually takes about five hours."
 		sys.exit(0)
 	
 	# Finished!
 	try:
-		inventory = json.loads(job.get_output().read())
+		listing = json.loads(job.get_output().read())
 	except ValueError:
 		print "Something went wrong interpreting the data Amazon sent!"
 		sys.exit(1)
 	
-	ls = {}
-	for archive in inventory['ArchiveList']:
-		ls[archive['ArchiveDescription']] = {
+	# Save job result
+	inventory = {}
+	for archive in listing['ArchiveList']:
+		inventory[archive['ArchiveDescription']] = {
 			'id': archive['ArchiveId'],
 			'size': int(archive['Size']),
 			'hash': archive['SHA256TreeHash']
 		}
 		
-	ls_present = '-'
-	inventory_job = ''
+	inventory_present = 'Inventory size'
+	inventory_retrieval_job_id = ''
 	write_config_file()
 	print "Imported a new inventory from Amazon."
+
+# If we just need to get inventory it retrieves inventory and exits 
+if arg.inventory:
+	sys.exit(0)
 
 # Let's upload!
 os.stat_float_times(False)
@@ -182,7 +229,7 @@ if arg.backup or arg.create:
 				vault.name = str(vault.name)
 
 				id = vault.concurrent_create_archive_from_file(path, archive)
-				ls[archive] = {
+				inventory[archive] = {
 					'id': id,
 					'size': size
 				}
@@ -209,7 +256,7 @@ if arg.backup or arg.create:
 		sys.exit(0)
 
 elif arg.delete:
-	for name, data in ls.iteritems():
+	for name, data in inventory.iteritems():
 		print 'Remove archive ID : ' + data['id']
 		try:
 			vault.delete_archive(data['id'])
@@ -227,13 +274,48 @@ elif arg.delete:
 			except:
 				print 'Cannot remove archive ID : %s' % data['id']
 
+	inventory = {}
+	inventory_present = ''
+	write_config_file()
+
 	print 'Removing vault...'
 	try:
 		vault.delete()
 		print 'Vault removed.'
-
-		ls = []
-		write_config_file()
 	except Exception as e:
 		print "We can't remove the vault now. Please wait some time and try again. You can also remove it from the AWS console, now that all archives have been removed."
 		print e.args
+
+elif arg.get:
+	try:
+		if not get_job_present:
+			for archive_name, archive_data in inventory.iteritems():
+				cur_job = vault.retrieve_archive(archive_data['id'])
+				get_jobs[archive_name] = {
+					'job_id' : cur_job.id,
+					'archive_name' : archive_name
+				}
+			get_job_present = "Job list:"
+			write_config_file()
+	except Exception as e:
+		print(traceback.format_exc())
+		sys.exit(1)
+	# We have a job, but it's not finished.
+	for job_name, job_data in get_jobs.iteritems():
+		cur_job = vault.get_job(job_data['job_id'])
+		if not cur_job.completed:
+			print "Waiting for an archive %s. This usually takes about five hours." % job_name
+			sys.exit(0)
+
+	for job_name, job_data in get_jobs.iteritems():
+		print "Start downloading " + job_name
+		cur_job.download_to_file(job_name)
+		print job_name + " has been successfully downloaded"
+		
+	
+	# Finished!	
+	get_jobs = {}
+
+	get_job_present = ''
+	write_config_file()
+	print "Imported all archives from vault."

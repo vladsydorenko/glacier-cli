8ec8281a2c91eb1275aa50d2e3e510c76669527a
Vlad Sydorenko <vladyslav.sydorenko@gmail.com>
2015-06-18 16:21:50 +0300

    Fixed error in Boto installing

--- setup_backup.sh
+++ setup_backup.sh
@@ -5,7 +5,8 @@ if [ ! -d "/usr/local/lib/python2.7/dist-packages/boto" ]; then
     git clone git://github.com/boto/boto.git
 	cd boto
 	yes | python setup.py install
-	rmdir boto
+	cd ..
+	rm -rf boto
 fi
 
 #Add script to crontab
@@ -14,12 +15,12 @@ crontab -l > mycron
 #get path to current folder
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #echo new cron into cron file
-echo "@weekly $DIR/sync-glacier.py -b > backup.log" >> mycron
+echo "@weekly $DIR/sync-glacier.py -b >> backup.log" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
 
 #Create vault and start syncronization for first time
-python sync-glacier.py -c >> backup.log
+python sync-glacier.py -c
 
 rm setup_backup.sh
